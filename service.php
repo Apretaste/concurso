9138c5d0fd203bb76fd5fbb03e631b208c624e90
Ricardo Jimenez <ricardo@apretaste.com>
2019-01-19 13:53:39 -0500

    Ready for NeoCore

--- service.php
+++ service.php
@@ -1,5 +1,5 @@
 <?php
-class Concurso extends Service
+class Service
 {
 	/**
 	 * Function executed when the service is called
@@ -7,147 +7,79 @@ class Concurso extends Service
 	 * @param Request $request
 	 * @return Response
 	 */
-	public function _main(Request $request)
-	{
-		$connection = new Connection();
-		$r = $connection->query("SELECT * from _concurso WHERE end_date >= now()");
-		if (isset($r[0]))
+	public function _main(Request $request, Response $response){
+		$contests = Connection::query("SELECT * from _concurso WHERE end_date >= now()");
 
-		{
-
-			foreach($r as $contest)
-			{
-				$contest->body = base64_decode($contest->body);
-				$contest->teaser = substr(strip_tags($contest->body),0,200);
-				$contest->end_date = substr($contest->end_date,0,strlen($contest->end_date)-3);
-			}
-
-			$r1 = $this->getWinners();
-
-			$response = new Response();
-			$response->setCache(10080); // for a week
-			$response->setResponseSubject("Concursos");
-			$response->createFromTemplate("basic.tpl",[
-				"contests" => $r,
-				"winners" => isset($r1[0])
-			]);
-
-			return $response;
+		if(empty($contests)){
+			$response->setTemplate('no-contests.ejs');
+			return;
 		}
 
-		$response = new Response();
-			$response->setResponseSubject("No hay concursos");
-			$response->createFromText("Temporalmente no tenemos concursos disponibles pero estamos trabajando en colocarlos.");
-			return $response;
+		foreach($contests as $contest){
+			unset($contest->body);
+			$contest->end_date = date_format((new DateTime($contest->end_date)),'d/m/Y - h:i a');
+		}
+		
+		$winner1 = Connection::query("SELECT * FROM _concurso WHERE end_date <= now() AND winner1 IS NOT NULL ORDER BY end_date DESC;");;
+		$response->setCache('day');
+		$response->setTemplate("basic.ejs",["contests" => $contests,"winners" => isset($winner1[0])]);
 	}
 
-	public function _ver(Request $request)
-	{
-		$connection = new Connection();
+	public function _ver(Request $request, Response $response){
+		$id = (isset($request->input->data->id))?$request->input->data->id:false;
 
-		$id = intval(trim($request->query));
-		$sql = "SELECT *, (end_date <= now()) as is_open from _concurso WHERE id = $id;";
-
-		$r = $connection->query($sql);
-
-		if (isset($r[0]))
-		{
-			$contest = $r[0];
-			$contest->body =$contest->body;
-			$contest->teaser = substr(strip_tags($contest->body),0,200);
-			$contest->end_date = substr($contest->end_date,0,strlen($contest->end_date)-3);
-
-			$contest->winners = false;
-
-			$winner1 = $this->utils->getPerson($contest->winner1);
-			$winner2 = $this->utils->getPerson($contest->winner2);
-			$winner3 = $this->utils->getPerson($contest->winner3);
-
-			$contest->winner1 = $winner1;
-			$contest->winner2 = $winner2;
-			$contest->winner3 = $winner3;
-
-			if ($winner1 !== false || $winner2 !== false || $winner2 !== false)
-				$contest->winners = [];
-
-			if ($winner1 !== false) $contest->winners[] = $winner1;
-			if ($winner2 !== false) $contest->winners[] = $winner2;
-			if ($winner3 !== false) $contest->winners[] = $winner3;
-
-			$images = $this->getContestImages($contest->id);
-
-			$di = \Phalcon\DI\FactoryDefault::getDefault();
-			$wwwroot = $di->get('path')['root'];
-
-			$imageList = [];
-			foreach ($images as $img)
-				$imageList = [$wwwroot."/public/contestsImages/$id/{$img['filename']}"];
-
-			$responseContent = ["contest" => $contest];
-			$response = new Response();
-			$response->setCache();
-			$response->setResponseSubject("Concurso");
-			$response->createFromTemplate("contest.tpl", $responseContent, $imageList);
-			return $response;
+		if(!$id){
+			$response->setTemplate('no-contests.ejs');
+			return;
 		}
 
-		return new Response();
-	}
+		$contest = Connection::query("SELECT *, (end_date >= now()) as is_open from _concurso WHERE id = $id");
 
-	private function getWinners()
-	{
-		$connection = new Connection();
-		$r = $connection->query("SELECT * FROM _concurso WHERE end_date <= now() AND winner1 is not null and winner1 <> '' order by end_date DESC limit 50;");
-		return $r;
-	}
-
-	public function _ganadores(Request $request)
-	{
-		$r = $this->getWinners();
-		if (isset($r[0]))
-		{
-			$contests = [];
-			foreach ($r as $contest)
-			{
-				$contest->winner1 = $this->utils->getPerson($contest->winner1);
-				$contest->winner2 = $this->utils->getPerson($contest->winner2);
-				$contest->winner3 = $this->utils->getPerson($contest->winner3);
-				$contest->end_date = substr($contest->end_date,0,strlen($contest->end_date)-3);
-
-				if ($contest->winner1 !== false || $contest->winner2 !== false || $contest->winner2 !== false)
-					$contests[] = $contest;
-			}
-
-			if (isset($contests[0]))
-			{
-				$response = new Response();
-				$response->setResponseSubject("Ganadores en concursos");
-				$response->createFromTemplate("winners.tpl", ["contests" => $contests]);
-				return $response;
-			}
+		if(empty($contest)){
+			$response->setTemplate('no-contests.ejs');
+			return;
 		}
 
-		return new Response();
+		$contest = $contest[0];
+		$contest->body = base64_decode($contest->body);
+		$contest->end_date = date_format((new DateTime($contest->end_date)),'d/m/Y - h:i a');
+
+		$winner1 = Utils::getPerson($contest->winner1);
+		$winner2 = Utils::getPerson($contest->winner2);
+		$winner3 = Utils::getPerson($contest->winner3);
+
+		if($winner1) $contest->winner1 = $winner1->username;
+		if($winner2) $contest->winner2 = $winner2->username;
+		if($winner3) $contest->winner3 = $winner3->username;
+
+		$response->setCache();
+		$response->setTemplate("contest.ejs", ["contest" => $contest]);
 	}
 
-	private function getContestImages($id)
-	{
-		$connection = new Connection();
-		$r = $connection->query("SELECT * FROM _concurso_images WHERE contest = '$id';");
+	public function _ganadores(Request $request, Response $response){
+		$cts = Connection::query("SELECT * FROM _concurso WHERE end_date <= now() AND winner1 IS NOT NULL ORDER BY end_date DESC;");;
+		if(empty($cts)){
+			$response->setTemplate('no-winners.ejs');
+			return;
+		}
+		$contests = [];
 
-		$di = \Phalcon\DI\FactoryDefault::getDefault();
-		$wwwroot = $di->get('path')['root'];
+		foreach ($cts as $contest){
+			unset($contest->body);
 
-		$images = [];
+			$winner1 = Utils::getPerson($contest->winner1);
+			$winner2 = Utils::getPerson($contest->winner2);
+			$winner3 = Utils::getPerson($contest->winner3);
 
-		if ($r !== false)
-		{
-			foreach ($r as $row)
-			{   $imageContent = file_get_contents($wwwroot."/public/contestsImages/$id/{$row->filename}");
-				$images[$row->filename] = ['filename' => $row->filename, 'type' => $row->mime_type, 'content' => base64_encode($imageContent)];
-			}
+			if($winner1) $contest->winner1 = $winner1->username;
+			if($winner2) $contest->winner2 = $winner2->username;
+			if($winner3) $contest->winner3 = $winner3->username;
+
+			$contest->end_date = date_format((new DateTime($contest->end_date)),'d/m/Y - h:i a');
+			$contests[] = $contest;
 		}
 
-		return $images;
+		$response->setTemplate('winners.ejs', ['contests' => $contests]);
+		return;
 	}
 }
