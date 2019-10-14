7765bfea76b4a8211c9a46313c36c062b14b77c2
salvipascual <salvi.pascual@gmail.com>
2019-10-14 15:38:34 -0400

    fix issues

--- service.php
+++ service.php
@@ -13,7 +13,7 @@ class Service
 	{
 		// get the list of contests
 		$contests = Connection::query("
-			SELECT id, end_date, title, winner3, prize1, prize2, prize3 
+			SELECT id, end_date, title, prize1, prize2, prize3 
 			FROM _concurso 
 			WHERE end_date >= NOW()
 			ORDER BY end_date ASC
@@ -94,45 +94,25 @@ class Service
 	public function _ganadores(Request $request, Response $response)
 	{
 		// get the winners list
-		$cts = Connection::query("
-			SELECT end_date, title, winner1, winner2, winner3, prize1, prize2, prize3 
-			FROM _concurso 
+		$contests = Connection::query("
+			SELECT 
+				end_date, title, prize1, prize2, prize3,
+				(SELECT username FROM person WHERE email = winner1) AS winner1,
+				(SELECT username FROM person WHERE email = winner2) AS winner2,
+				(SELECT username FROM person WHERE email = winner3) AS winner3
+			FROM _concurso
 			WHERE end_date <= NOW() 
 			AND winner1 IS NOT NULL 
 			ORDER BY end_date DESC
 			LIMIT 10");
 
 		// message for empty winners
-		if(empty($cts)) {
+		if(empty($contests)) {
 			return $response->setTemplate('message.ejs', [
 				"header"=>"No hay concursos",
 				"icon"=>"sentiment_very_dissatisfied",
 				"text" => "No tenemos los resultados de ningún concurso de momento. Si un concurso terminó y los resultados aún no aparecen, por favor comuníquese con el soporte."
 			]);
-		}
-
-		// get list of winners 
-		$contests = [];
-		foreach ($cts as $contest){
-			// get the winner 1 if exist
-			if($contest->winner1) {
-				$w = Connection::query("SELECT username FROM person WHERE email = '{$contest->winner1}'");
-				$contest->winner1 = empty($w) ? "" : $w[0]->username;
-			}
-
-			// get the winner 2 if exist
-			if($contest->winner2) {
-				$w = Connection::query("SELECT username FROM person WHERE email = '{$contest->winner2}'");
-				$contest->winner2 = empty($w) ? "" : $w[0]->username;
-			}
-
-			// get the winner 1 if exist
-			if($contest->winner3) {
-				$w = Connection::query("SELECT username FROM person WHERE email = '{$contest->winner3}'");
-				$contest->winner3 = empty($w) ? "" : $w[0]->username;
-			}
-
-			$contests[] = $contest;
 		}
 
 		// send data to the view
