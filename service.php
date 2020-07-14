4c2b1363c4a56d89b8f6fcac8230f102aaf1232a
Salvi Pascual <salvi.pascual@gmail.com>
2020-07-13 23:52:39 -0400

    add apretaste contests

--- service.php
+++ service.php
@@ -10,17 +10,15 @@ class Service
 	/**
 	 * List of contests
 	 *
+	 * @author salvipascual
 	 * @param Request $request
 	 * @param Response $response
-	 *
-	 * @throws \Framework\Alert
-	 * @author salvipascual
 	 */
-	public function _main(Request $request, Response &$response)
+	public function _main(Request $request, Response $response)
 	{
 		// get the list of contests
 		$contests = Database::query('
-			SELECT id, end_date, title, prize1, prize2, prize3 
+			SELECT id, end_date, title, prize1, prize2, prize3, is_internal
 			FROM _concurso 
 			WHERE end_date >= NOW()
 			ORDER BY end_date ASC');
@@ -34,37 +32,12 @@ class Service
 			]);
 		}
 
+		// complete challenge
+		Challenges::complete('view-current-contests', $request->person->id);
+
 		// send data to the view
 		$response->setCache('day');
 		$response->setTemplate('home.ejs', ['contests' => $contests]);
-
-		Challenges::complete('view-current-contests', $request->person->id);
-	}
-
-	/**
-	 * Get contest by id
-	 *
-	 * @param $id
-	 * @return mixed|null
-	 */
-	public static function getContest($id)
-	{
-		$contest = Database::query("
-			SELECT *,
-				(SELECT username FROM person WHERE person.id = _concurso.winner_1) as username1,
-				(SELECT username FROM person WHERE person.id = _concurso.winner_2) as username2,
-				(SELECT username FROM person WHERE person.id = _concurso.winner_3) as username3
-			FROM _concurso WHERE id = $id");
-
-		if (isset($contest[0])) {
-			$contest = $contest[0];
-			$contest->winner1 = $contest->username1;
-			$contest->winner2 = $contest->username2;
-			$contest->winner3 = $contest->username3;
-			return $contest;
-		}
-
-		return null;
 	}
 
 	/**
@@ -74,16 +47,16 @@ class Service
 	 * @param Response $response
 	 * @author salvipascual
 	 */
-	public function _ver(Request $request, Response &$response)
+	public function _ver(Request $request, Response $response)
 	{
 		// get the contest id
 		$id = isset($request->input->data->id) ? $request->input->data->id : '';
 
-		// get the contest
-		$contest = self::getContest($id);
+		// get contest from the database
+		$contest = Database::queryFirst("SELECT * FROM _concurso WHERE id = $id");
 
 		// message for empty contests
-		if ($contest === null) {
+		if (empty($contest)) {
 			return $response->setTemplate('message.ejs', [
 				'header' => 'Concurso no encontrado',
 				'icon' => 'sentiment_very_dissatisfied',
@@ -106,31 +79,25 @@ class Service
 	 * @param Response $response
 	 * @author salvipascual
 	 */
-	public function _ganadores(Request $request, Response &$response)
+	public function _ganadores(Request $request, Response $response)
 	{
-		for ($i = 1; $i < 4; $i++) {
-			Database::query("UPDATE _concurso SET winner{$i} = null WHERE winner{$i} = ''");
-			Database::query("UPDATE _concurso SET winner_{$i} = null WHERE winner_{$i} = 250378");
-			Database::query("UPDATE _concurso SET winner{$i} = (SELECT email FROM person WHERE person.id = winner_{$i}) WHERE winner{$i} is null");
-			Database::query("UPDATE _concurso SET winner_{$i} = (SELECT id FROM person WHERE person.email = winner{$i}) WHERE winner_{$i} is null");
-		}
-
 		// get the winners list
 		$contests = Database::query("
 			SELECT 
 				end_date, title, prize1, prize2, prize3,
-				(SELECT username FROM person WHERE email = winner1) AS winner1,
-				(SELECT avatar FROM person WHERE email = winner1) AS winner1avatar,
-				(SELECT avatarcolor FROM person WHERE email = winner1) AS winner1aColor,
-				(SELECT username FROM person WHERE email = winner2) AS winner2,
-				(SELECT avatar FROM person WHERE email = winner2) AS winner2avatar,
-				(SELECT avatarColor FROM person WHERE email = winner2) AS winner2aColor,
-				(SELECT username FROM person WHERE email = winner3) AS winner3,
-				(SELECT avatar FROM person WHERE email = winner3) AS winner3avatar,
-				(SELECT avatarcolor FROM person WHERE email = winner3) AS winner3aColor
+				(SELECT username FROM person WHERE id = winner_1) AS winner_1,
+				(SELECT avatar FROM person WHERE id = winner_1) AS winner_1avatar,
+				(SELECT avatarcolor FROM person WHERE id = winner_1) AS winner_1aColor,
+				(SELECT username FROM person WHERE id = winner_2) AS winner_2,
+				(SELECT avatar FROM person WHERE id = winner_2) AS winner_2avatar,
+				(SELECT avatarColor FROM person WHERE id = winner_2) AS winner_2aColor,
+				(SELECT username FROM person WHERE id = winner_3) AS winner_3,
+				(SELECT avatar FROM person WHERE id = winner_3) AS winner_3avatar,
+				(SELECT avatarcolor FROM person WHERE id = winner_3) AS winner_3aColor
 			FROM _concurso
-			WHERE end_date <= NOW() 
-			AND (winner1 IS NOT NULL || winner1 <> '') 
+			WHERE end_date <= NOW()
+			AND is_internal = 1
+			AND (winner_1 IS NOT NULL || winner_1 <> '') 
 			ORDER BY end_date DESC
 			LIMIT 10");
 
