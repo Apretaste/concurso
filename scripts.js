ed62cee53f307f6afb5532660348c456e1f19c23
kumahacker <kumahavana@gmail.com>
2020-08-09 17:03:53 -0300

    update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,40 @@
 $(document).ready(function () {
 	$('.tabs').tabs();
+	$('.modal').modal();
 });
+
+var share;
+function init(contest) {
+	share = {
+		text: 'CONCURSO: ' + contest.body.substr(0, 100),
+		icon: 'star',
+		send: function () {
+			apretaste.send({
+				command: 'PIZARRA PUBLICAR',
+				redirect: false,
+				callback: {
+					name: 'toast',
+					data: 'El concurso fue compartido en Pizarra'
+				},
+				data: {
+					text: $('#message').val(),
+					image: '',
+					link: {
+						command: btoa(JSON.stringify({
+							command: 'CONCURSO VER',
+							data: {
+								id: contest.id
+							}
+						})),
+						icon: share.icon,
+						text: share.text
+					}
+				}
+			})
+		}
+	};
+}
+
+function toast(message){
+	M.toast({html: message});
+}
